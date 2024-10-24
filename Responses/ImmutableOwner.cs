49867d7a56eac30840754e45a4f9d310549600d4
matamike <matamike1990@gmail.com>
2024-10-24 17:21:35 +0300

    Creation of ImmutableOwner Display template

---
+++ Responses/ImmutableOwner.cs
@@ -0,0 +1,5 @@
+﻿using TechnicoApplication.Models;
+
+namespace TechnicoApplication.Responses;
+
+public record ImmutableOwner(string Email,string Name,string Surname,string Address,string VatNumber,string PhoneNumber, string password, Owner.OwnerType ownerType);
