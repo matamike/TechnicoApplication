0fd97f198880902445eec79e809efb5a79e55a4d
matamike <matamike1990@gmail.com>
2024-10-24 20:00:46 +0300

    CreatePropertyService created.

---
+++ Services/CreatePropertyService.cs
@@ -0,0 +1,10 @@
+﻿using TechnicoApplication.Models;
+
+namespace TechnicoApplication.Services;
+
+public class CreatePropertyService{
+
+    public void CreateProperty(Property property, Owner owner){
+        PropertyHandler.CreateProperty(property, owner);
+    }
+}
