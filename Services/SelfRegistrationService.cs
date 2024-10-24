9e47fc57093fe4b9fbe8fe6f92d0872805852a31
matamike <matamike1990@gmail.com>
2024-10-24 17:23:58 +0300

    SelfRegistrationService Creation

---
+++ Services/SelfRegistrationService.cs
@@ -0,0 +1,9 @@
+﻿using TechnicoApplication.Models;
+
+namespace TechnicoApplication.Services;
+
+public class SelfRegistrationService{
+    public void RegisterOwner(Owner newOwner){
+        OwnerHandler.CreateOwner(newOwner);
+    }
+}
