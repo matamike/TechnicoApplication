328ca2eacceaf20667d4c019caa8bc06e194e4bd
matamike <matamike1990@gmail.com>
2024-10-29 19:16:33 +0200

    ImmutableResponseStatus allows to have a simple response status with/without a Printable in place to view.

---
+++ Responses/ImmutableResponseStatus.cs
@@ -0,0 +1,9 @@
+﻿using TechnicoApplication.Models;
+
+namespace TechnicoApplication.Responses;
+
+public record ImmutableResponseStatus(bool status, string? message, IPrintable? printable = null){
+    public void PrintResponseStatus() => Console.WriteLine($"Status: {status}");
+    public void PrintResponseMessage() => Console.WriteLine($"Message: {message}");
+    public void PrintObjectData() => printable?.PrintSelf();
+};
